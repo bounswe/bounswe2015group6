37e7a313e79a79341e8778f7fcba9b0205e918d5
Hamza Öztürk <hamza.ozturk@tikle.com>
2015-05-03 15:43:49 +0300

    Added three test cases for binary plus operation.

--- src/test/java/MathematicalOperationsTest.java
+++ src/test/java/MathematicalOperationsTest.java
@@ -1,3 +1,21 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MathematicalOperationsTest {
     
+    static MathematicalOperations mathematicalOperations;
+    
+    @BeforeClass
+    public static void init() {
+        mathematicalOperations = new MathematicalOperations();
+    }
+    
+    @Test
+    public void testBinaryPlus() {
+        assertEquals(4,mathematicalOperations.binaryPlus(4,0));
+        assertEquals(0,mathematicalOperations.binaryPlus(-1,1));
+        assertEquals(85,mathematicalOperations.binaryPlus(28,57));
+    }
 }
