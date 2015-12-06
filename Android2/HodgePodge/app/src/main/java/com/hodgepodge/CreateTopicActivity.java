f05409e9cf5149e71354cd551a2010d2ccb30779
mahmutsami106 <sami.yagmur@boun.edu.tr>
2015-12-06 23:18:30 +0200

    Prevent users to enter blank title

--- Android2/HodgePodge/app/src/main/java/com/hodgepodge/CreateTopicActivity.java
+++ Android2/HodgePodge/app/src/main/java/com/hodgepodge/CreateTopicActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
@@ -30,45 +31,55 @@ public class CreateTopicActivity extends AppCompatActivity {
     public void createTopic(View view) throws JSONException {
         final EditText titleEditText = (EditText) findViewById(R.id.titleEditText);
         final EditText tagsEditText = (EditText) findViewById(R.id.tagsEditText);
+        final TextView errorCreateTopicTextView = (TextView) findViewById(R.id.errorCreateTopicTextView);
         SharedPreferences user = getSharedPreferences("UserInfo", 0);
 
-        JSONObject jsonParams = new JSONObject();
-        jsonParams.put("ownerId", user.getString("ID", ""));
-        jsonParams.put("title", titleEditText.getText().toString());
+        String convertedTitle = titleEditText.getText().toString().replaceAll("\\s+", " ");
+        titleEditText.setText(convertedTitle);
+        if ( titleEditText.getText().toString().equals(" ") || titleEditText.getText().toString().isEmpty() ) {
+            tagsEditText.setText("");
+            errorCreateTopicTextView.setVisibility(View.VISIBLE);
+            errorCreateTopicTextView.setText("Please, Enter a Title");
 
-        // Taking tags which are  seperated by space
-        String delims = "[ ]+";
-        String[] tags = tagsEditText.getText().toString().split(delims);
-        JSONArray tagsJson = new JSONArray(Arrays.asList(tags));
-        jsonParams.put("tags", tagsJson);
+        } else {
+            JSONObject jsonParams = new JSONObject();
+            jsonParams.put("ownerId", user.getString("ID", ""));
+            jsonParams.put("title", titleEditText.getText().toString());
+            // Taking tags which are  seperated by space
+            String delims = "[ ]+";
+            String[] tags = tagsEditText.getText().toString().split(delims);
+            JSONArray tagsJson = new JSONArray(Arrays.asList(tags));
+            jsonParams.put("tags", tagsJson);
 
-        StringEntity entity = null;
-        try {
-            entity = new StringEntity(jsonParams.toString());
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            StringEntity entity = null;
+            try {
+                entity = new StringEntity(jsonParams.toString());
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            ServiceClient.post(this, "topic/create", entity, "application/json",
+                    new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            System.out.println("Create Topic Sucess");
+                            errorCreateTopicTextView.setText("The topic was created");
+                            //Send user to homepage
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                            System.out.println(error.getMessage());
+                            Log.d("TOPIC CREATE ERROR", error.getMessage().toString());
+
+                            //if (error.getMessage().equals("Usernameexist")) {
+                            //  errorTextView.setText("Passwords do not match. Please try again.");
+                            //  usernameField.setText("");
+                            //}
+                        }
+                    });
+
         }
-
-
-        ServiceClient.post(this, "topic/create", entity, "application/json",
-                new AsyncHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                        System.out.println("Create Topic Sucess");
-                        //Send user to homepage
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                        System.out.println(error.getMessage());
-                        Log.d("TOPIC CREATE ERROR" ,error.getMessage().toString());
-
-                        //if (error.getMessage().equals("Usernameexist")) {
-                        //  errorTextView.setText("Passwords do not match. Please try again.");
-                        //  usernameField.setText("");
-                        //}
-                    }
-                });
     }
 }
 
