274388d02439544f79b4cc6e73812262942ed48a
oralemre <atpeo93@hotmail.com>
2015-12-13 19:42:00 +0200

    Error cases added in create_relation

--- Android2/HodgePodge/app/src/main/java/com/hodgepodge/CreateRelationActivity.java
+++ Android2/HodgePodge/app/src/main/java/com/hodgepodge/CreateRelationActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -29,49 +30,61 @@ public class CreateRelationActivity extends AppCompatActivity {
     public void createRelation(View view) throws JSONException {
         final EditText topic1Text = (EditText) findViewById(R.id.editTxtTopic1);
         final EditText topic2Text = (EditText) findViewById(R.id.editTxtTopic2);
+        final TextView errorText =  (TextView) findViewById(R.id.errorTextViewRelation);
         final Context context = this;
         ServiceClient.get("topic/title/" + topic1Text.getText(), null, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 try {
-                    source = response.getInt("id");
-                    // URL = URL + Integer.toString(response.getInt("id")) + "&dest=";
-                    ServiceClient.get("topic/title/" + topic2Text.getText(), null, new JsonHttpResponseHandler() {
-                        @Override
-                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                            try {
-                                dest = response.getInt("id");
+                    if(response.getInt("id") != 0){
+                        source = response.getInt("id");
+                        // URL = URL + Integer.toString(response.getInt("id")) + "&dest=";
+                        ServiceClient.get("topic/title/" + topic2Text.getText(), null, new JsonHttpResponseHandler() {
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                                try {
+                                    if(response.getInt("id") != 0){
+                                        dest = response.getInt("id");
 
-                                ServiceClient.post(getApplicationContext(), URL + source + "&dest=" + dest, null, "application/json",
-                                        new AsyncHttpResponseHandler() {
-                                            @Override
-                                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                                                System.out.println("Create Relation Sucess");
-                                                Intent intent = new Intent(context, HomePageActivity.class);
-                                                startActivity(intent);
-                                                //Send user to homepage
-                                            }
-                                            @Override
-                                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                                                System.out.println(error.getMessage());
-                                                //if (error.getMessage().equals("Usernameexist")) {
-                                                //  errorTextView.setText("Passwords do not match. Please try again.");
-                                                //  usernameField.setText("");
-                                                //}
-                                            }
-                                        });
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+                                        ServiceClient.post(getApplicationContext(), URL + source + "&dest=" + dest, null, "application/json",
+                                                new AsyncHttpResponseHandler() {
+                                                    @Override
+                                                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                                        System.out.println("Create Relation Sucess");
+                                                        Intent intent = new Intent(context, HomePageActivity.class);
+                                                        startActivity(intent);
+                                                        //Send user to homepage
+                                                    }
+                                                    @Override
+                                                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                                        System.out.println(error.getMessage());
+                                                        //if (error.getMessage().equals("Usernameexist")) {
+                                                        //  errorTextView.setText("Passwords do not match. Please try again.");
+                                                        //  usernameField.setText("");
+                                                        //}
+                                                    }
+                                                });
+                                    }else{
+                                        errorText.setVisibility(View.VISIBLE);
+                                    }
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
                             }
 
-                        }
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                                super.onFailure(statusCode, headers, responseString, throwable);
+                                throwable.printStackTrace(System.out);
+                            }
+                        });
+                    }else{
+                        errorText.setVisibility(View.VISIBLE);
+                    }
 
-                        @Override
-                        public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                            super.onFailure(statusCode, headers, responseString, throwable);
-                            throwable.printStackTrace(System.out);
-                        }
-                    });
 
                 } catch (JSONException e) {
                     e.printStackTrace();
