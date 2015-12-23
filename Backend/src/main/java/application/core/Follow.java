e1a7536621b2bc106730c0bfe0ff8ba48b122a2e
unknown <hbaristr@gmail.com>
2015-12-23 19:57:46 +0200

    Follow class is documented

--- Backend/src/main/java/application/core/Follow.java
+++ Backend/src/main/java/application/core/Follow.java
@@ -6,6 +6,9 @@ import javax.persistence.*;
 @Table(name = "follow")
 public class Follow {
 
+    /**
+     * id of "follow" relationship
+     */
     @Id
     @GeneratedValue
     private int id;
@@ -25,18 +28,34 @@ public class Follow {
         this.id = id;
     }
 
+    /**
+     *
+     * @return id of user who is followed
+     */
     public int getFollowed() {
         return followedId;
     }
 
+    /**
+     *
+     * @param followed set id of user who is followed
+     */
     public void setFollowed(int followed) {
         this.followedId = followed;
     }
 
+    /**
+     *
+     * @return id of user who follows
+     */
     public int getFollower() {
         return followerId;
     }
 
+    /**
+     *
+     * @param follower id of user who will be added as follower
+     */
     public void setFollower(int follower) {
         this.followerId = follower;
     }
