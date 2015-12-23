4eb5055c6275322b21ab639333cc188e3ee49b14
unknown <hbaristr@gmail.com>
2015-12-23 19:03:32 +0200

    class modified

--- Backend/src/main/java/application/core/RegisteredUser.java
+++ Backend/src/main/java/application/core/RegisteredUser.java
@@ -1,10 +1,16 @@
 package application.core;
 
+import javax.persistence.Column;
+
 /**
  * Created by Baris on 23.12.2015.
  */
 public class RegisteredUser extends User{
 
-    
+    @Column(name = "loginDate")
+    String loginDate;
+
+    @Column(name = "logoutDate")
+    String logoutDate;
 
 }
